4b6c86a3756513e221aa1a5e9d55335537104512
aerorweqwe <128631282+aerorweqwe@users.noreply.github.com>
2024-06-01 13:54:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
     el: '.items, .item, .contactUs',
     data: {
         products:[
-            {id:1, title:"TAG 853", short_text:"Eggplant Determinate Black handsome Round. ", image:"tag853.png", desc:"The plant is an indeterminate, erect bush that reaches 2–4 feet tall when fully grown. Eggplants have an extensive, fibrous root system. Flowers are borne singly or in clusters in leaf axils"},
-            {id:2, title:"TAG 855", short_text:"Eggplant Determinate Eggplant diamond Round.", image:"tag855.png", desc:"The plant is an indeterminate, erect bush that reaches 2–4 feet tall when fully grown. Eggplants have an extensive, fibrous root system. Flowers are borne singly or in clusters in leaf axils"},
-            {id:3, title:"TAG 816", short_text:"Eggplant Determinate Purple miracle Round.", image:"tag816.png", desc:"The plant is an indeterminate, erect bush that reaches 2–4 feet tall when fully grown. Eggplants have an extensive, fibrous root system. Flowers are borne singly or in clusters in leaf axils"},
-            {id:4, title:"TAG 834", short_text:"Eggplant Determinate 'Banana' Round", image:"tag834.png", desc:"The plant is an indeterminate, erect bush that reaches 2–4 feet tall when fully grown. Eggplants have an extensive, fibrous root system. Flowers are borne singly or in clusters in leaf axils"},
-            {id:5, title:"TAG 848", short_text:"Eggplant Determinate Small Round", image:"tag848.png", desc:"The plant is an indeterminate, erect bush that reaches 2–4 feet tall when fully grown. Eggplants have an extensive, fibrous root system. Flowers are borne singly or in clusters in leaf axils"}
+            {id:1, title:"TAG 853", short_text:"Onion Bulb. ", image:"tag853.png", desc:"Basic onion. Not everyone will like it raw, but it is excellent caramelized or fried along with barbecue. This bow can be used in any unclear situation"},
+            {id:2, title:"TAG 855", short_text:"Onion White .", image:"tag855.png", desc:"When raw, these onions have a milder taste than onions - you can thinly slice them into a salad or put them on a burger. And if you rinse with cold water, the sharpness and bitterness will disappear completely. Replaces regular onions in almost any recipe"},
+            {id:3, title:"TAG 816", short_text:"Onion Red.", image:"tag816.png", desc:"Sweeter than onion and white. It is added raw to dishes, stewed and grilled, and marinated. The main feature is the texture: even after heat treatment, the onion holds its shape and is slightly crunchy. This can be both a plus and a minus"},
+            {id:4, title:"TAG 834", short_text:"Onion Yalta", image:"tag834.png", desc:"The sweetest and juiciest of all - ideal in vegetable salads. This is a seasonal onion: it is harvested only in summer and early autumn, and is not stored for long."},
+            {id:5, title:"TAG 848", short_text:"Onion The Shawl", image:"tag848.png", desc:"A vegetable from the world of haute cuisine. The taste is sweet and rich, but almost without bitterness. Can be used as a substitute for onions in most recipes or finely chopped into sauce or salad dressing."}
         ],
         product:[],
         cart:[],
